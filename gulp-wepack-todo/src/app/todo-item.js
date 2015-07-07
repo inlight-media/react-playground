a74d9a856e2dac26aca064d6e2679f8f23bcfd39
Will McClellan <mcclellan.will@gmail.com>
2015-07-07 15:27:13 +1000

    Todo editing

--- gulp-wepack-todo/src/app/todo-item.js
+++ gulp-wepack-todo/src/app/todo-item.js
@@ -7,8 +7,14 @@ var Button = require('../shared/button');
 
 var TodoItem = React.createClass({
   toggleChecked: toggleChecked,
+  handleSetEditing: handleSetEditing,
+  handleEdit: handleEdit,
+  handleEditChange: handleEditChange,
+  getInitialState: getInitialState,
   render: render
 });
+
+var ENTER_KEY = 13;
 
 function toggleChecked() {
   var props = this.props;
@@ -18,13 +24,67 @@ function toggleChecked() {
   handler(id);
 }
 
-function render() {
+function handleSetEditing() {
+  var self = this;
+  var props = self.props;
+  var id = props.id;
+  var handler = props.onSetEditableItem;
+  console.log('TodoItem :: handleSetEditing', { id: id });
+  // The cb is necessary here because focus doesn't work until
+  // the dom el does not have { display: 'none }. So we wait
+  // for the component to re-render and then set the foucs
+  handler(id, function cb() {
+    React.findDOMNode(self.refs.newText).focus();
+  });
+}
+
+function handleEdit(e) {
   var props = this.props;
   var id = props.id;
+  var handler = props.onEdit;
+  var newText = e.target.value.trim()
+
+  // only handle enter key events
+  if (e.keyCode !== ENTER_KEY) {
+    return;
+  }
+
+  e.preventDefault();
+
+  console.log('handle edit todo', {
+    id: id,
+    newText: newText
+  });
+
+  handler(id, newText);
+}
+
+function handleEditChange(e) {
+  this.setState({
+    editText: e.target.value
+  });
+}
+
+function getInitialState() {
+  return {
+    editText: this.props.text
+  }
+}
+
+function render() {
+  var props = this.props;
+  var state = this.state;
+  var id = props.id;
   var checked = props.checked;
+  var editing = props.editing;
   var text = props.text;
+  // TODO update show/hide with external css ~ 07-07-2015 @willmcclellan
   var textStyle = {
-    textDecoration: checked && 'line-through'
+    textDecoration: checked && 'line-through',
+    display: (editing ? 'none' : 'inline-block')
+  };
+  var inputStyle = {
+    display: (editing ? 'inline-block' : 'none')
   };
 
   return (
@@ -33,7 +93,8 @@ function render() {
 	type="checkbox"
 	checked={checked}
 	onChange={this.toggleChecked} />
-      <span style={textStyle} onClick={this.toggleChecked}>{text}</span>
+      <span style={textStyle} onClick={this.handleSetEditing}>{text}</span>
+      <input style={inputStyle} type="text" ref="newText" onChange={this.handleEditChange} onKeyDown={this.handleEdit} value={state.editText} autoFocus={true} />
       <Button handler={props.onRemove.bind(null, id)}>&times;</Button>
     </div>
   );
--- gulp-wepack-todo/src/app/todo.js
+++ gulp-wepack-todo/src/app/todo.js
@@ -14,6 +14,8 @@ var Todo = React.createClass({
   buildTodoItems: buildTodoItems,
   todoCount: todoCount,
   toggleChecked: toggleChecked,
+  editTodo: editTodo,
+  setEditing: setEditing,
   setFilter: setFilter,
   getInitialState: getInitialState,
   render: render
@@ -51,15 +53,53 @@ function buildTodoItems() {
     ? _.filter(todos, { checked: filter })
     : todos;
   return _.map(filteredTodos, function mapTodos(todo) {
+    // editing is true when the state editing Id is
+    // equal to the current todo Id
+    var editing = (state.editing === todo.id);
     return (
       // the spread syntax (...obj) allows us to apply
       // all the props of the object as properties
       // to the Component or element
       <TodoItem {...todo}
 	key={todo.id}
+	editing={editing}
+	onEdit={self.editTodo}
+	onSetEditableItem={self.setEditing}
 	onCheckToggle={self.toggleChecked}
 	onRemove={self.remove} />
     )
+  });
+}
+
+function setEditing(id, callback) {
+  console.log('Todo :: setEditing', { id: id });
+  this.setState({
+    editing: id
+  }, callback);
+}
+
+function editTodo(id, newText) {
+  console.log('Todo :: editTodo', {
+    id: id,
+    newText: newText
+  });
+
+  newText = newText || '';
+
+  var todos = this.state.todos;
+  var todo = _.find(todos, { id: id });
+  todo.text = newText.trim();
+
+  // if the todo is edited as empty
+  // just remove it from the collection
+  if (todo.text === '') {
+    this.remove(id);
+    return;
+  }
+
+  this.setState({
+    editing: undefined, // reset editing state
+    todos: todos
   });
 }
 
@@ -85,6 +125,7 @@ function setFilter(filter) {
 function getInitialState() {
   return {
     todos: [],
+    editing: undefined, // should ref the id of currently editing item
     filter: undefined
   }
 }

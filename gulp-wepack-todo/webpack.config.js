8ac03c882a8512cd40ca83e31717528df97f4df6
Will McClellan <mcclellan.will@gmail.com>
2015-07-07 15:10:24 +1000

    fix indentation on webpack config

--- gulp-wepack-todo/webpack.config.js
+++ gulp-wepack-todo/webpack.config.js
@@ -13,30 +13,30 @@ var webpack = require('webpack');
 module.exports = {
   cache: true,
   entry: [
-		'webpack-dev-server/client?http://localhost:5000', // WebpackDevServer host and port
-		'webpack/hot/only-dev-server',
-		'./src/app/index.js'
-	],
+    'webpack-dev-server/client?http://localhost:5000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server',
+    './src/app/index.js'
+  ],
   output: {
     path: path.join(__dirname, './dist')
   },
   module: {
-		// Loaders are transformations that can be applied to
-		// certain files using a regex pattern. It replaces
-		// the need for gulp task. Here we're using ones for
-		// jsx compilation (see React docs) and to faciliate
-		// are react-hot-loader for live reloading
+    // Loaders are transformations that can be applied to
+    // certain files using a regex pattern. It replaces
+    // the need for gulp task. Here we're using ones for
+    // jsx compilation (see React docs) and to faciliate
+    // are react-hot-loader for live reloading
     loaders: [
       // { test: /\.css$/, loader: 'style!css' },
       {
-				include: path.join(__dirname, 'src'),
-				test: /\.js$/,
-				loader: 'react-hot!jsx'
-			}
+	include: path.join(__dirname, 'src'),
+	test: /\.js$/,
+	loader: 'react-hot!jsx'
+      }
     ]
   },
   plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin()
-	]
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 };

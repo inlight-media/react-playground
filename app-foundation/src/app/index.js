b28a768d0ab9661ef0078fa0015b4925d3a83be1
Oliver Benns <oliverbenns@gmail.com>
2015-07-13 16:44:59 +1000

    Put main area in view

--- app-foundation/src/app/index.js
+++ app-foundation/src/app/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import Header from '../components/header';
+import Main from '../components/main';
+
 import styles from './style.css';
 import reset from './reset.css';
 
 React.render(
 	<div className={styles.app}>
 		<Header />
+		<Main />
 	</div>
 	, document.body);

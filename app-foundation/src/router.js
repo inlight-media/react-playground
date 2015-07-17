1b64a61fd3d33f4c915cb33a5654952344a65351
Oliver Benns <oliverbenns@gmail.com>
2015-07-17 10:34:17 +1000

    Adjusted router to one file

--- app-foundation/src/router.js
+++ app-foundation/src/router.js
@@ -5,19 +5,26 @@ import { history } from 'react-router/lib/HashHistory';
 // Components
 import App from '../app/'
 import NotFound from 'not-found'
-
-// Routes
-// import Locations from './locations'
-// <Locations />
-
-import Zones from './zones'
-console.log('Zones', Zones);
+import Zones from 'zones';
+import LocationsList from 'locations/list';
+import LocationsNew from 'locations/new';
 
 React.render((
 	<Router history={ history }>
 		<Route path="/" component={ App }>
-			<Zones />
+
+			// Locations
+			<Route path="locations" component={ LocationsList }>
+				<Route path="list" component={ LocationsList } />
+				<Route path="new" component={ LocationsNew } />
+			</Route>
+
+			// Zones
+			<Route path="zones" component={ Zones } />
+
 			<Route path="*" component={ NotFound } />
 		</Route>
 	</Router>
 ), document.body);
+
+

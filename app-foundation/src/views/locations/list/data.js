74e2da6a463103253d0c16be9fb4c5e566ed0e5f
Oliver Benns <oliverbenns@gmail.com>
2015-07-21 10:30:57 +1000

    Update locations list to have separate data file

---
+++ app-foundation/src/views/locations/list/data.js
@@ -0,0 +1,47 @@
+/* Temp Data */
+module.exports = [
+	{
+		link:"http://www.google.com",
+		title: "Google",
+		description: "A great search engine",
+		buttons: [
+			{
+				theme: "primary",
+				href: "/accept",
+				label: "Lorem"
+			},
+			{
+				theme: "success",
+				href: "/deny",
+				label: "Lorem"
+			}
+		],
+		icon: "house"
+	},
+	{
+		link:"http://www.bing.com",
+		title: "Bing",
+		description: "An ok search engine",
+		buttons: [
+			{
+				theme: "primary",
+				href: "/deny",
+				label: "Lorem"
+			}
+		],
+		icon: "house"
+	},
+	{
+		link:"http://www.ask.com",
+		title: "Ask",
+		description: "Non-existent",
+		buttons: [
+			{
+				theme: "secondary",
+				href: "/view",
+				label: "Lorem"
+			}
+		],
+		icon: "house"
+	}
+];
--- app-foundation/src/views/locations/list/index.js
+++ app-foundation/src/views/locations/list/index.js
@@ -3,71 +3,19 @@ import styles from './style.css';
 
 // @TODO: Resolve module directories in webpack config so we don't have to keep backtracking to components.
 import List from 'list';
-import Dropdown from 'dropdown';
+import Title from 'title';
+import Button from 'button';
 
-var items = [
-	{
-		link:"http://www.google.com",
-		title: "Google",
-		description: "A great search engine",
-		buttons: [
-			{
-				theme: "primary",
-				href: "/accept",
-				label: "Lorem"
-			},
-			{
-				theme: "success",
-				href: "/deny",
-				label: "Lorem"
-			}
-		]
-	},
-	{
-		link:"http://www.bing.com",
-		title: "Bing",
-		description: "An ok search engine",
-		buttons: [
-			{
-				theme: "primary",
-				href: "/deny",
-				label: "Lorem"
-			}
-		]
-	},
-	{
-		link:"http://www.ask.com",
-		title: "Ask",
-		description: "Non-existent",
-		buttons: [
-			{
-				theme: "secondary",
-				href: "/view",
-				label: "Lorem"
-			}
-		]
-	}
-];
-
-var options = [
-	{
-		value: 'one',
-		label: 'One'
-	},
-	{
-		value: 'two',
-		label: 'Two'
-	},
-];
-
-var defaultOption = 'two';
+import items from './data'
 
 export default class LocationList extends Component {
 	render() {
 		return (
 			<div className={ styles.container }>
+				<Title label="Locations">
+					<Button theme="primary">Test</Button>
+				</Title>
 				<List items={ items } />
-				<Dropdown options={ options } value={ defaultOption } />
 			</div>
 		);
 	}

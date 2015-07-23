f7976743a1afc4a791b87a67c37135c5649e9050
Oliver Benns <oliverbenns@gmail.com>
2015-07-23 16:43:09 +1000

    Dropdown styles

--- app-foundation/src/components/button-set/index.js
+++ app-foundation/src/components/button-set/index.js
@@ -15,7 +15,8 @@ export default class ButtonSet extends Component {
 	render() {
 		var classes = classNames(
 			styles.container,
-			this.props.className ? this.props.className : ''
+			this.props.className ? this.props.className : '',
+			this.props.final ? styles.final : ''
 		);
 
 		return (
--- app-foundation/src/components/dropdown/index.js
+++ app-foundation/src/components/dropdown/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import styles from './style.css';
 
+import Icon from 'icon';
+
 export default class Dropdown extends Component {
 	constructor(props) {
 		super(props);
@@ -30,6 +32,7 @@ export default class Dropdown extends Component {
 				<select className={ styles.select } value={ this.state.value } onChange={ this.handleChange.bind(this) }>
 					{ this.buildOptions(this.props.options) }
 				</select>
+				<Icon theme="arrow-down" className={ styles.icon } />
 			</div>
 		);
 
